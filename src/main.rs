f88b4c8edef2915ff2dc8b16fc3813a87cd1816c
ficeloo <tcros@FIXE-THEO.localdomain>
2026-02-26 20:41:03 +0100

    suppression fn setuup *inutile*, mise en place du setup dans le add_setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/***** MAIN.RS *****/
+
+use bevy::prelude::*;
+
+mod camera;
+mod player;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (camera::spawn_camera, player::spawn_player))
+        .run()
 }

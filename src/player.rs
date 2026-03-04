9f07a88993d094934269054c6fe9906f67feb917
Theophile Cros <tcros@f6r12s14.clusters.42paris.fr>
2026-03-04 13:16:38 +0100

    modification de l'inertie et de la propulsion, bug de recul resolu

--- src/player.rs
+++ src/player.rs
@@ -4,12 +4,12 @@ use bevy::prelude::*;
 
 const P_SPRITE_SIZE: f32 = 0.3;
 const P_SPRITE_PATH: &str = "stellar_drifter.png";
-const P_SPEED: f32 = 15.0;
+const P_SPEED: f32 = 600.0;
 const P_ROT_SPEED: f32 = 5.0;
 
 #[derive(Component)]
 pub struct Movement {
-    pub velocity: f32,
+    pub velocity: Vec3,
 }
 
 #[derive(Component)]
@@ -23,33 +23,32 @@ pub struct PlayerBundle {
 }
 
 pub fn move_player(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    keyboard_input: Res<Input<KeyCode>>,
     mut player_query: Query<(&mut Transform, &mut Movement), With<Player>>,
     delta_time: Res<Time>,
 ) {
     let delta_time = delta_time.delta_seconds();
 
+
     for (mut transform, mut movement) in player_query.iter_mut() {
         let direction = transform.up();
 
-        // Principe d'inertie dans le vide
-        transform.translation += direction * movement.velocity * delta_time;
-
-        if keyboard_input.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]) {
-            transform.translation += direction * P_SPEED * delta_time;
-            movement.velocity += P_SPEED;
+        if keyboard_input.any_pressed([KeyCode::W, KeyCode::Up]) {
+            movement.velocity += direction * P_SPEED * delta_time;
         }
-        if keyboard_input.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]) {
-            transform.translation -= direction * (P_SPEED / 20.0) * delta_time;
-            movement.velocity -= P_SPEED / 20.0;
+        if keyboard_input.any_pressed([KeyCode::S, KeyCode::Down]) {
+            movement.velocity -= ((direction * P_SPEED * delta_time) / 4.0).clamp_length(0.0, 50.0);
         }
-        if keyboard_input.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]) {
+        if keyboard_input.any_pressed([KeyCode::A, KeyCode::Left]) {
             transform.rotate_z(P_ROT_SPEED * delta_time);
         }
-        if keyboard_input.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]) {
+        if keyboard_input.any_pressed([KeyCode::D, KeyCode::Right]) {
             transform.rotate_z(-P_ROT_SPEED * delta_time);
         }
-        movement.velocity = (movement.velocity - 5.0).clamp(0.0, 1000.0);
+        movement.velocity = (movement.velocity * 0.98).clamp_length(0.0, 500.0);
+
+        // Principe d'inertie dans le vide
+        transform.translation += movement.velocity * delta_time;
     }
 }
 
@@ -64,7 +63,7 @@ pub fn spawn_player(mut commands: Commands, assets_server: Res<AssetServer>) {
 
     let player_bundle = PlayerBundle {
         player: Player,
-        movement: Movement { velocity: 0.0 },
+        movement: Movement { velocity: Vec3::ZERO },
         sprite: ship_sprite,
     };
     commands.spawn(player_bundle);

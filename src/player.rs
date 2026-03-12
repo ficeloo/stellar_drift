1aa21cd05670f6cd596b0c7dfaeb49db65577aae
ficeloo <tcros@FIXE-THEO.localdomain>
2026-03-12 02:17:40 +0100

    ajout monde toroidale (tp entre les fenetres)

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 /***** PLAYER.RS *****/
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 const P_SPRITE_SIZE: f32 = 0.3;
 const P_SPRITE_PATH: &str = "stellar_drifter.png";
@@ -22,12 +22,31 @@ pub struct PlayerBundle {
     pub sprite: SpriteBundle,
 }
 
+fn is_player_oob(transform: &mut Transform, half_width: f32, half_height: f32) {
+    if transform.translation.x > half_width + 40.0 {
+        transform.translation.x = -(half_width + 40.0);
+    } else if transform.translation.x < -(half_width + 40.0) {
+        transform.translation.x = half_width + 40.0;
+    }
+    if transform.translation.y > half_height + 40.0 {
+        transform.translation.y = -(half_height + 40.0);
+    } else if transform.translation.y < -(half_height + 40.0) {
+        transform.translation.y = half_height + 40.0;
+    }
+}
+
 pub fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
     mut player_query: Query<(&mut Transform, &mut Movement), With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
     delta_time: Res<Time>,
 ) {
+    let Ok(window) = window_query.get_single() else {
+        return;
+    };
     let delta_time = delta_time.delta_seconds();
+    let half_width = window.width() / 2.0;
+    let half_height = window.height() / 2.0;
 
     for (mut transform, mut movement) in player_query.iter_mut() {
         let direction = transform.up();
@@ -44,10 +63,12 @@ pub fn move_player(
         if keyboard_input.any_pressed([KeyCode::D, KeyCode::Right]) {
             transform.rotate_z(-P_ROT_SPEED * delta_time);
         }
-        movement.velocity = (movement.velocity * 0.98).clamp_length(0.0, 9000.0);
+        movement.velocity = (movement.velocity * 0.98).clamp_length(0.0, 15000.0);
 
         // Principe d'inertie dans le vide
         transform.translation += movement.velocity * delta_time;
+
+        is_player_oob(&mut transform, half_width, half_height);
     }
 }
 

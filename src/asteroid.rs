d7a41ed43f4b98e6a951ad0f4e6f34046e342bbf
ficeloo <tcros@FIXE-THEO.localdomain>
2026-03-26 23:02:21 +0100

    ajout spawn differentes tailles asteroides

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -3,18 +3,34 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 use rand::prelude::*;
 
+use crate::game::LevelState;
 use crate::player::{Health, Movement};
 
 const A_S1_SPRITE_PATH: &str = "asteroid-1.png";
-const A_S1_SPRITE_SIZE: f32 = 0.3;
+const A_S1_SPRITE_SIZE: f32 = 0.2;
+
+const A_S2_SPRITE_PATH: &str = "asteroid-2.png";
+const A_S2_SPRITE_SIZE: f32 = 0.4;
+
+const A_S3_SPRITE_PATH: &str = "asteroid-3.png";
+const A_S3_SPRITE_SIZE: f32 = 0.6;
+
 const A_MAX_SPEED: f32 = 150.0;
 
 #[derive(Component)]
 pub struct Asteroid;
 
+#[derive(Component, Clone, Copy)]
+pub enum AsteroidSize {
+    Large,
+    Medium,
+    Small,
+}
+
 #[derive(Bundle)]
 pub struct AsteroidBundle {
     pub asteroid: Asteroid,
+    pub size: AsteroidSize,
     pub health: Health,
     pub movement: Movement,
     pub sprite: SpriteBundle,
@@ -38,33 +54,50 @@ fn generate_random_velocity(max_speed: f32) -> Vec3 {
     Vec3::new(vel_x, vel_y, 0.0).normalize() * rand_speed
 }
 
-pub fn spawn_asteroid_small(
+fn create_asteroid(
+    size: AsteroidSize,
+    window: &Window,
+    asset_server: &AssetServer,
+) -> AsteroidBundle {
+    let (sprite_path, sprite_size) = match size {
+        AsteroidSize::Large => (A_S3_SPRITE_PATH, A_S3_SPRITE_SIZE),
+        AsteroidSize::Medium => (A_S2_SPRITE_PATH, A_S2_SPRITE_SIZE),
+        AsteroidSize::Small => (A_S1_SPRITE_PATH, A_S1_SPRITE_SIZE),
+    };
+
+    let asteroid_asset = asset_server.load(sprite_path);
+
+    let asteroid_sprite = SpriteBundle {
+        texture: asteroid_asset,
+        transform: Transform {
+            translation: generate_random_position(window.width() / 2.0, window.height() / 2.0),
+            scale: Vec3::splat(sprite_size),
+            ..default()
+        },
+        ..default()
+    };
+
+    AsteroidBundle {
+        asteroid: Asteroid,
+        size,
+        health: Health::new(1),
+        movement: Movement::new(generate_random_velocity(A_MAX_SPEED)),
+        sprite: asteroid_sprite,
+    }
+}
+
+pub fn spawn_asteroid(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    assets_server: Res<AssetServer>,
+    // level_state: Res<LevelState>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let Ok(window) = window_query.get_single() else {
         return;
     };
 
-    let asteroid_asset = asset_server.load(A_S1_SPRITE_PATH);
-
-    let asteroid_sprite = SpriteBundle {
-        texture: asteroid_asset,
-        transform: Transform {
-            translation: generate_random_position(window.width() / 2.0, window.height() / 2.0),
-            scale: Vec3::splat(A_S1_SPRITE_SIZE),
-            ..default()
-        },
-        ..default()
-    };
-
-    let asteroid_bundle = AsteroidBundle {
-        asteroid: Asteroid,
-        health: Health::new(1),
-        movement: Movement::new(generate_random_velocity(A_MAX_SPEED)),
-        sprite: asteroid_sprite,
-    };
-
-    commands.spawn(asteroid_bundle);
+    // Il faut que je rajoute le level_state afin d'augmenter le nombre d'asteroides
+    for _ in 0..(0 + 3) {
+        commands.spawn(create_asteroid(AsteroidSize::Large, window, &assets_server));
+    }
 }

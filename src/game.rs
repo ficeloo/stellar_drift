ac0fbc1615691b51d637c33a0fa4eb45c4999325
ficeloo <tcros@FIXE-THEO.localdomain>
2026-03-29 01:58:59 +0100

    ajout levelstate dans game.rs

--- src/game.rs
+++ src/game.rs
@@ -6,3 +6,16 @@ use bevy::prelude::*;
 pub struct LevelState {
     pub level: u8,
 }
+
+pub fn is_entity_oob(transform: &mut Transform, half_width: f32, half_height: f32) {
+    if transform.translation.x > half_width + 40.0 {
+        transform.translation.x = -(half_width + 40.0);
+    } else if transform.translation.x < -(half_width + 40.0) {
+        transform.translation.x = half_width + 40.0;
+    }
+    if transform.translation.y > half_height + 40.0 {
+        transform.translation.y = -(half_height + 40.0);
+    } else if transform.translation.y < -(half_height + 40.0) {
+        transform.translation.y = half_height + 40.0;
+    }
+}
